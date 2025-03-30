0f64e603627f24b4654d74b076f22f6099594e86
Shinovon <arman200711@gmail.com>
2025-03-31 01:56:59 +0500

    chat info form wip

--- src/ChatInfoForm.java
+++ src/ChatInfoForm.java
@@ -1,13 +1,35 @@
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.StringItem;
+
+import cc.nnproject.json.JSONObject;
 
 public class ChatInfoForm extends MPForm {
 
+	String id;
+	
 	public ChatInfoForm(String id) {
 		super(id);
+		this.id = id;
 	}
 
 	void loadInternal(Thread thread) throws Exception {
-		// TODO Auto-generated method stub
+		JSONObject peer = MP.getPeer(id, true);
 		
+		id = peer.getString("id");
+		boolean user = id.charAt(0) != '-';
+		String name = MP.getName(peer);
+		setTitle(name);
+		
+		StringItem s;
+		
+		s = new StringItem(null, name);
+		s.setFont(MP.medPlainFont);
+		s.setLayout(Item.LAYOUT_NEWLINE_BEFORE | Item.LAYOUT_NEWLINE_AFTER);
+		append(s);
+		
+		JSONObject fullInfo = (JSONObject) MP.api("getFullInfo&id=".concat(id));
+		
+		append(fullInfo.format(0));
 	}
 
 }
