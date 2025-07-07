e8c5b5cfa9e825105ea5c054ea2821e98ceb874a
Shinovon <arman200711@gmail.com>
2025-07-07 22:02:40 +0500

    optimize fillGrad a little bit

--- src/ViewCanvas.java
+++ src/ViewCanvas.java
@@ -448,11 +448,11 @@ public class ViewCanvas extends Canvas implements Runnable, LangConstants {
 	 * @param c2 Bottom color.
 	 */
 	public static void fillGrad(Graphics g, int x, int y, int w, int h, int c1, int c2) {
+		final int c2_RB = c2 & 0x00FF00FF;
+		final int c2_AG_org = c2 & 0xFF00FF00;
 		for (int i = 0; i < h; i++) {
 //			g.setColor(blend(c2, c1, i * 255 / h));
 			int v = (i * 255 / h) & 0xFF;
-			final int c2_RB = c2 & 0x00FF00FF;
-			final int c2_AG_org = c2 & 0xFF00FF00;
 			g.setColor((((c2_AG_org) + ((((c1 >>> 8) & 0x00FF00FF) - ((c2_AG_org) >>> 8)) * v)) & 0xFF00FF00)
 					| ((c2_RB + ((((c1 & 0x00FF00FF) - c2_RB) * v) >> 8)) & 0x00FF00FF));
 			g.drawLine(x, y + i, x + w, y + i);
